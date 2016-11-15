c55c8508af8f9fbeea8f362555f3973080422ada
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-11-15 00:36:08 +0000

    changed routes

--- homework3/config/routes.rb
+++ homework3/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :websites
   resources :daniels
-  root 'daniels#index'
+  #root 'daniels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
